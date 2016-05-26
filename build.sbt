a95d9dc2116a01202999e80f8f3c6f495714bc81
nlw0 <nwerneck@gmail.com>
2016-05-26 19:47:56 +0200

    yaya

--- build.sbt
+++ build.sbt
@@ -4,4 +4,10 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
+/*libraryDependencies ++= List(
+  "com.typesafe.akka" %% "akka-actor" % "2.4.6",
+  "com.typesafe.akka" %% "akka-http-core" % "2.4.6",
+  "com.typesafe.akka" %% "akka-http-experimental" % "2.4.6"
+)*/
+
 javaOptions in run += "-Djava.library.path=/home/n.werneck/share/OpenCV/java"
--- src/main/scala/RandServer.scala
+++ src/main/scala/RandServer.scala
@@ -1,3 +1,57 @@
-class RandServer {
+/*import akka.actor.ActorSystem
+import akka.actor.Props
+import scala.concurrent.duration._
+import akka.util.Timeout
+import akka.pattern.ask
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import spray.json.DefaultJsonProtocol._
+import scala.io.StdIn
 
-}
+object WebServer {
+
+  case class Bid(userId: String, bid: Int)
+  case object GetBids
+  case class Bids(bids: List[Bid])
+
+  // these are from spray-json
+  implicit val bidFormat = jsonFormat2(Bid)
+  implicit val bidsFormat = jsonFormat1(Bids)
+
+  def main(args: Array[String]) {
+    implicit val system = ActorSystem()
+    implicit val materializer = ActorMaterializer()
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.dispatcher
+
+    val auction = system.actorOf(Auction.props, "auction")
+
+    val route =
+      path("auction") {
+        put {
+          parameter("bid".as[Int], "user") { (bid, user) =>
+            // place a bid, fire-and-forget
+            auction ! Bid(user, bid)
+            complete((StatusCodes.Accepted, "bid placed"))
+          }
+        }
+        get {
+          implicit val timeout: Timeout = 5.seconds
+
+          // query the actor for the current auction state
+          val bids: Future[Bids] = (auction ? GetBids).mapTo[Bids]
+          complete(bids)
+        }
+      }
+
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ ⇒ system.terminate()) // and shutdown when done
+
+  }
+}*/
