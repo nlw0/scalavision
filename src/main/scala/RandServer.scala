adcfe13ebae517842d53bc4b0f37f691dea0e581
nlw0 <nwerneck@gmail.com>
2016-06-08 02:24:23 +0200

    opencv + akka-http worked!

--- src/main/scala/RandServer.scala
+++ src/main/scala/RandServer.scala
@@ -2,9 +2,11 @@ import akka.actor.ActorSystem
 import akka.stream.scaladsl._
 import akka.util.ByteString
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, MediaTypes}
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+import org.opencv.core.{Core, MatOfByte}
+import org.opencv.imgcodecs.Imgcodecs
 
 import scala.util.Random
 import scala.io.StdIn
@@ -12,7 +14,7 @@ import scala.io.StdIn
 object RandServer {
 
   def main(args: Array[String]) {
-
+    System.loadLibrary(Core.NATIVE_LIBRARY_NAME)
     implicit val system = ActorSystem()
     implicit val materializer = ActorMaterializer()
     // needed for the future flatMap/onComplete in the end
@@ -32,6 +34,25 @@ object RandServer {
               // transform each number to a chunk of bytes
               numbers.map(n => ByteString(s"$n\n"))
             )
+          )
+        }
+      } ~ path("test") {
+        get {
+          val imageFilename = "/home/nlw/buska.jpg"
+          val img = Imgcodecs.imread(imageFilename)
+          val xx = Array[Byte](0.toByte, 0.toByte, 255.toByte)
+          img.put(10, 10, xx)
+          img.put(10, 11, xx)
+          img.put(11, 10, xx)
+          img.put(11, 11, xx)
+          var yowza = new MatOfByte
+
+
+
+          Imgcodecs.imencode(".jpg", img, yowza)
+
+          complete(
+            HttpEntity(MediaTypes.`image/jpeg`, yowza.toArray)
           )
         }
       }
